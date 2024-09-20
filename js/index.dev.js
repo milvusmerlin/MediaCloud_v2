af30c77dc9cd128bd0667b7bb3245dd4e02d903f
mika <mika@milvus.co.kr>
2024-09-20 21:01:50 +0900

    PC 이미지 경로 수정

--- js/index.dev.js
+++ js/index.dev.js
@@ -1,20 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const userItems = document.querySelectorAll(".user_ab ul li");
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var userItems = document.querySelectorAll(".user_ab ul li");
   console.log(userItems);
-  const user_ab = document.querySelector(".user_ab > a > img");
-  const slide1img = document.querySelector(".rec_prd img");
-
-  const slide1img_bg = document.querySelector(".userA");
-  const ddi_banner_bg = document.querySelector(".ddi_banner img");
-  const ddibanner = document.querySelector(".ddi_banner img");
-  const user = { name: "" };
-
-  userItems.forEach((item) => {
-    item.addEventListener("click", (e) => {
+  var user_ab = document.querySelector(".user_ab > a > img");
+  var slide1img = document.querySelector(".rec_prd img");
+  var slide1img_bg = document.querySelector(".userA");
+  var ddi_banner_bg = document.querySelector(".ddi_banner img");
+  var ddibanner = document.querySelector(".ddi_banner img");
+  var user = {
+    name: ""
+  };
+  userItems.forEach(function (item) {
+    item.addEventListener("click", function (e) {
       user.name = e.target.innerText;
-
-      userItems.forEach((li) => li.classList.remove("active"));
+      userItems.forEach(function (li) {
+        return li.classList.remove("active");
+      });
       item.classList.add("active");
+
       if (user.name === "사용자 A") {
         slide1img.src = "./image/slide1_img2.png";
         ddibanner.src = "./image/ddibanner_a.png";
--- js/prd.dev.js
+++ js/prd.dev.js
@@ -1,19 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const userItems = document.querySelectorAll(".user_ab ul li");
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var userItems = document.querySelectorAll(".user_ab ul li");
   console.log(userItems);
-  const user_ab = document.querySelector(".user_ab > a > img");
-
-  const user = { name: "" };
-  // 상품 페이지 띠배너
+  var user_ab = document.querySelector(".user_ab > a > img");
+  var user = {
+    name: ""
+  }; // 상품 페이지 띠배너
   // const prd_ddi_banner_bg = document.querySelector(".prd_ddibanner a img");
-  const prd_ddibanner = document.querySelector(".prd_ddibanner img");
 
-  userItems.forEach((item) => {
-    item.addEventListener("click", (e) => {
+  var prd_ddibanner = document.querySelector(".prd_ddibanner img");
+  userItems.forEach(function (item) {
+    item.addEventListener("click", function (e) {
       user.name = e.target.innerText;
-
-      userItems.forEach((li) => li.classList.remove("active"));
+      userItems.forEach(function (li) {
+        return li.classList.remove("active");
+      });
       item.classList.add("active");
+
       if (user.name === "사용자 A") {
         user_ab.src = "./image/user.png";
         prd_ddibanner.src = "./image/prd_ddibanner_a.png";
